471757398fabd8a233230e498adfcd1ececdb354
Hayato Kato <kato.hayato@willgate.co.jp>
2021-06-09 18:27:15 +0530

    Update CORS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -31,7 +31,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'forum-prod-api.herokuapp.com']
 CORS_ALLOW_ALL_ORIGINS = False
-CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'http://127.0.0.1:3000', 'https://forum-prod-frontend.herokuapp.com']
+CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'http://127.0.0.1:3000',
+                        'https://forum-prod-frontend.herokuapp.com', 'https://twitter-prod-frontend.herokuapp.com']
 
 # Application definition
 
