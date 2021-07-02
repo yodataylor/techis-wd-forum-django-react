21d45b19835dbea690548f46d368ed93aab1a409
kayato <h.kato345@gmail.com>
2021-07-02 23:13:50 +0530

    Remove about

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router";
 import Home from "./containers/Home";
-import About from "./containers/About";
 
 const Router = () => {
   return (
