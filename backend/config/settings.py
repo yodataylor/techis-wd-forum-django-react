f9586ca29195508e5bb42d7a62e214b4b2d7ee6f
sajid-techis <sajid@techis.io>
2022-07-27 04:32:08 +0530

    fixed createstore

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-ov#r^^&xv&^0vmc(zj&h_t^$*52@8jicn=%*z*@s-=li!s_p@=
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'forum-prod-api.herokuapp.com']
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'http://127.0.0.1:3000', 'https://forum-prod-frontend.herokuapp.com']
 
 # Application definition
--- frontend/src/reducks/store/store.js
+++ frontend/src/reducks/store/store.js
@@ -1,22 +1,26 @@
-import { applyMiddleware, combineReducers, configureStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import { logger } from 'redux-logger';
-import thunk from 'redux-thunk';
+import {
+  applyMiddleware,
+  combineReducers,
+  createStore as configureStore,
+} from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { logger } from "redux-logger";
+import thunk from "redux-thunk";
 
-import { PostsReducer } from '../posts/reducers';
+import { PostsReducer } from "../posts/reducers";
 
 const rootReducer = combineReducers({
-    posts: PostsReducer
+  posts: PostsReducer,
 });
 
 export default function configureStores(preloadedState) {
-    const middlewares = [logger, thunk];
-    const middlewareEnhancer = applyMiddleware(...middlewares);
+  const middlewares = [logger, thunk];
+  const middlewareEnhancer = applyMiddleware(...middlewares);
 
-    const enhancers = [middlewareEnhancer];
-    const composedEnhancers = composeWithDevTools(...enhancers);
+  const enhancers = [middlewareEnhancer];
+  const composedEnhancers = composeWithDevTools(...enhancers);
 
-    const store = configureStore(rootReducer, preloadedState, composedEnhancers);
+  const store = configureStore(rootReducer, preloadedState, composedEnhancers);
 
-    return store;
+  return store;
 }
