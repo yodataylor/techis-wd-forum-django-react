a055c9fbf9b6b894fd5e26d68c8ef6551a6f3b6d
ddeveza <ddeveza@ymail.com>
2022-07-27 05:46:13 +0800

    rename the configureStore as createStore since we already use the name configureStore as function

--- frontend/src/reducks/store/store.js
+++ frontend/src/reducks/store/store.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { applyMiddleware, combineReducers, configureStore as createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { logger } from 'redux-logger';
 import thunk from 'redux-thunk';
