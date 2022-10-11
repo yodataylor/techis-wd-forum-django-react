be918e174fc2d8eb1255177f9ffb452e87b800e5
sajid-techis <sajid@techis.io>
2022-10-11 21:58:42 +0530

    Deleted Heroku Database

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -82,20 +82,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# Heroku Database
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'dch7n72frg031v',
-#         'USER': 'ldpelxievviuhf',
-#         'PORT': 5432,
-#         'HOST': 'ec2-23-23-164-251.compute-1.amazonaws.com',
-#         'PASSWORD': '8a9353d3c2aa7a6baaf128ed3c6fc0a4ede1cccfec1ccc0222e089e0871b8975',
-#     }
-# }
+
 
 # Local Database
 DATABASES = {
@@ -105,7 +93,7 @@ DATABASES = {
     }
 }
 
-# Heroku PostgreSQL Database
+
 
 
 # Password validation
